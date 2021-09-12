9fceb52e3031715aa8c49397859136766b6a42a8
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-12 16:38:46 -0700

    process.env.PORT

--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 Based on a tutorial by the Net Ninja
 Deploying to GCP app engine https://www.youtube.com/watch?v=HgpCjChgjoQ&list=PL42xwJRIG3xCtmOrJAQFR5sIJFKIJ9MEn&index=2
+didnt work at first but had to set port to process.env.PORT to work.
+had to add txt verification file to aws blogs.anhonestobserver.com to register domain
+had to copy name servers from app engine to where i bought the domain name (aws)
+ssl in aws
--- server.js
+++ server.js
@@ -10,9 +10,12 @@ const app = express();
 // connect to mongodb & listen for requests
 const dbURI = `mongodb+srv://andrewcbuensalida:${process.env.DB_PW}@cluster0.okm5c.mongodb.net/net-ninja?retryWrites=true&w=majority`;
 
+const PORT = process.env.PORT || 5000;
 mongoose
 	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then((result) => app.listen(5000, () => console.log("Listening to 5000")))
+	.then((result) =>
+		app.listen(PORT, () => console.log(`Listening to port ${PORT}`))
+	)
 	.catch((err) => console.log(err));
 
 // register view engine
