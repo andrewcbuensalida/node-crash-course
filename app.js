fe9e2bc7e67d5847a4498669975ee5f8b3ad4d6d
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-09-11 17:12:58 -0700

    comment

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get("/about", (req, res) => {
 // blog routes
 app.use("/blogs", blogRoutes);
 
-// 404 page
+// 404 page. could either be app.get('*') OR app.all('*') OR
 app.use((req, res) => {
 	res.status(404).render("404", { title: "404" });
 });
